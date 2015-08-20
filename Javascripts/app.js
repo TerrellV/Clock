b8549d8e71562c69abf3e7970dc0c7429de6adc2
Terrell V (MirPresT) <terrell.vest@gmail.com>
2015-08-20 14:00:15 -0700

    added animation for clocks and timer progress

--- Javascripts/app.js
+++ Javascripts/app.js
@@ -1,54 +1,155 @@
-var app = angular.module("myApp", [] );
+var app = angular.module("myApp", []);
 
-app.controller('timerController', function($scope){
+app.controller('timerController', function($scope) {
 
-    $
-        var totalMinutes = 1;//parseInt($('h1').html());
-        var totalSeconds = totalMinutes * 60;
-        var totalMilliseconds =  totalSeconds * 1000;
-        $scope.s = 60 || $scope.enteredSeconds;
-        $scope.m = parseInt(totalMinutes.toString().slice(0)) - 1; // minus one if seconds === 0 because they were not entered;
-        var finished = false;
+    var multipliers = {
+        "hours": 3600000,
+        "minutes": 60000,
+        "seconds": 1000
+    }
 
-        var timeoutID = window.setTimeout(complete, [totalMilliseconds]);
+    var paused = false;
+    var timeouts = [];
+    var enteredProps;
 
-        function tick(){
-            if ( finished ) {
-                $scope.s = "DONE!";
-                return false;
+    // ON CLICK OF START BUTTON
+    $scope.start = function() {
+        $scope.hideStart = true;
+        $scope.running = true;
+        $scope.showRunningScreen();
+        // $scope.updateTime($scope.time);
+        $scope.startTime( $scope.time );
+    }
+
+    // ON CLICK OF STOP BUTTON
+    $scope.pauseApp = function() {
+        paused = true;
+        $scope.paused = true;
+        console.log($scope.displaySeconds,$scope.displayMinutes,$scope.displayHours);
+        $("#loading-inner").css("animation-play-state", "paused");
+        $(".clock-hand").css("animation-play-state", "paused");
+
+        $("#btn-resume").attr('disabled','disabled');
+        function delayResumeButton () {
+            document.getElementById('btn-resume').removeAttribute('disabled');
+        }
+        var delayResumeButtonID = window.setTimeout(delayResumeButton, 750);
+    }
+    // ON CLICK OF RESUME BUTTON
+    $scope.resume = function() {
+        // build new object here to pass into timer function again with past values
+        $scope.pausedValues = {
+            'hours': $scope.displayHours,
+            'minutes': $scope.displayMinutes,
+            'seconds': $scope.displaySeconds
+        }
+        $scope.paused = false;
+        paused = false;
+        $scope.startTime( $scope.pausedValues );
+        function delayResume () {
+            $("#loading-inner").css("animation-play-state", "running");
+        }
+        var delayResumeID = setTimeout(delayResume, 700);
+    }
+
+    // DEFINE : begin timmer
+    $scope.startTime = function( timeValues ) {
+
+        $(".clock-hand").css("animation-play-state", "running");
+
+        enteredProps = Object.getOwnPropertyNames($scope.time);
+        var filteredProps = enteredProps.filter(function(prop) {
+            return $scope.time[prop] !== 0;
+        });
+        $scope.time.totalMs = 0;
+
+        filteredProps.map(function(prop) {
+            $scope.time.totalMs += $scope.time[prop] * multipliers[prop];
+        });
+
+        $scope.displayHours = timeValues.hours || 0;
+        $scope.displayMinutes = timeValues.minutes || 0;
+        $scope.displaySeconds = timeValues.seconds || 0;
+
+        function onEachSecondDo() {
+
+            if ( paused ) {
+                return 'done'
             }
-            if ( $scope.s === 0 ) {
-                $scope.s = 60;
-                $scope.m--;
+
+            if ($scope.displayHours === 0 && $scope.displayMinutes ===0 && $scope.displaySeconds === 0) {
+                $(".clock-hand").css("animation-play-state", "paused");
+                finished = false;
+                return '';
             }
-            $scope.s--;
+            if ($scope.displayMinutes === 0 && $scope.displaySeconds === 0) {
+                $scope.displayMinutes = 60;
+                $scope.displayHours--;
+            }
+
+            if ($scope.displaySeconds === 0) {
+                $scope.displaySeconds = 60;
+                $scope.displayMinutes--;
+            }
+            $scope.displaySeconds--;
             $scope.$apply();
-            show();
+            console.log('h', $scope.displayHours, 'min', $scope.displayMinutes, 'sec', $scope.displaySeconds);
+            ticker();
         }
 
-        function show () {
-            var timeoutS = window.setTimeout(tick, [1000] );
+        function ticker() {
+            var timeoutID = window.setTimeout(onEachSecondDo, [1000]);
+            timeouts.push(timeoutID);
         }
 
-        // when master timer runs out
-        function complete() {
-            finished = true;
-        }
+        ticker();
 
-    show();
+        // give loading bar an animation duration
+        $("#loading-inner").css("animation-duration", $scope.time.totalMs +'ms');
 
-});
-
-var max_chars = 2;
-
-$('#minutes').keydown( function(e){
-    if ($(this).val().length >= 2) {
-        $(this).val($(this).val().substr(0, max_chars+1));
     }
+
+    // DEFINE on click of start - set css -
+    $scope.showRunningScreen = function(){
+        // angular styles.
+        $scope.clockContainer = {
+            "top": "130px"
+        };
+        $scope.bigClock = {
+            "height": "200px",
+            "width": "200px"
+        };
+        $scope.bigHandM = {
+            "height": "60px",
+            "width": "6px",
+            "bottom": "95px",
+            "transform-origin": "center 57px",
+            "animation": "clockHandSpin 60s linear 50ms infinite both"
+        };
+        $scope.bigHandS = {
+            "height": "60px",
+            "width": "6px",
+            "bottom": "95px",
+            "transform-origin": "center 57px",
+            "animation": "clockHandSpin 1s linear 50ms infinite both"
+        };
+    }
+
 });
 
-$('#minutes').keyup( function(e){
-    if ($(this).val().length >= 2) {
-        $(this).val($(this).val().substr(0, max_chars+1));
+// limit input of text to two characters and only numbers
+function maxLengthCheck(object) {
+    if (object.value.length > object.maxLength)
+        object.value = object.value.slice(0, object.maxLength)
+}
+
+function isNumeric(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
+    var regex = /[0-9]|\./;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
     }
-});
+}
