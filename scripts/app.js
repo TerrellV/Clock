8a8b08bc9b716b819085a9cd0a149393d8bf77be
Terrell V (MirPresT) <terrell.vest@gmail.com>
2015-08-23 11:01:18 -0700

    changed path name of partials to subcontent to hopefully fix path issues with github pages. Changing folder name seemed to work when issue was encountered before.

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,10 @@ app.config(function($routeProvider){
     // fix routes so that they work with gihub pages
     $routeProvider
         .when('/', {
-            templateUrl: '/partials/stopwatch.html'
+            templateUrl: '/subcontent/test.html'
         })
         .when('/stopwatch', {
-            templateUrl: '/partials/test.html'
+            templateUrl: '/subcontent/stopwatch.html'
         })
 })
 
